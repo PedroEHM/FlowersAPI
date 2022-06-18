db3a7207108121f2b9a8b4f6e0fef09f105a0e2b
Pedro Hernández <phernandezm1@unicartagena.edu.co>
2022-06-18 10:49:24 -0500

    Creación del proyecto

---
+++ src/main/java/com/flowers/FlowersApiApplication.java
@@ -0,0 +1,13 @@
+package com.flowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowers/FlowersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
